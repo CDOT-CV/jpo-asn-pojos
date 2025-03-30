7268597b3d42c0cd0adbf3a3c26c6465ae69cbb2
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-03-30 01:31:46 -0600

    PDC message frame test xml/json

--- jpo-asn-j2735-2024/src/test/java/us/dot/its/jpo/asn/j2735/r2024/ProbeDataConfig/ProbeDataConfigMessageMessageFrameTest.java
+++ jpo-asn-j2735-2024/src/test/java/us/dot/its/jpo/asn/j2735/r2024/ProbeDataConfig/ProbeDataConfigMessageMessageFrameTest.java
@@ -2,10 +2,10 @@ package us.dot.its.jpo.asn.j2735.r2024.ProbeDataConfig;
 
 import org.junit.jupiter.api.Test;
 import us.dot.its.jpo.asn.j2735.r2024.BaseSerializeTest;
-import us.dot.its.jpo.asn.j2735.r2024.MapData.MapDataMessageFrame;
 
 import java.io.IOException;
 
+import static net.javacrumbs.jsonunit.JsonMatchers.jsonEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.xmlunit.matchers.CompareMatcher.isIdenticalTo;
@@ -19,13 +19,18 @@ public class ProbeDataConfigMessageMessageFrameTest extends BaseSerializeTest<Pr
     public void xmlRoundTripTest() throws IOException {
         final String xml = loadResource("/us/dot/its/jpo/asn/j2735/r2024/ProbeDataConfig/message_frame/pdc_mf.xml");
         var msg = fromXml(xml);
-
-        String json = toJson(msg);
-        System.out.println(json);
-
         assertThat(msg, notNullValue());
         final String roundTripXml = toXml(msg);
         assertThat(roundTripXml,
                 isIdenticalTo(xml).ignoreWhitespace().ignoreElementContentWhitespace());
+    }
+
+    @Test
+    public void jsonRoundTripTest() throws IOException {
+        final String json = loadResource("/us/dot/its/jpo/asn/j2735/r2024/ProbeDataConfig/message_frame/pdc_mf.json");
+        var msg = fromJson(json);
+        assertThat(msg, notNullValue());
+        final String roundTripJson = toJson(msg);
+        assertThat(roundTripJson, jsonEquals(json));
     }
 }
